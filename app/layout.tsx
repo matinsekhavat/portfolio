33e2dc9da7b49478f743ab241772833439e96a54
matinsekhavat <matinhyphen@gmail.com>
2024-07-15 17:14:19 +0330

    setup project Grid system

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,6 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import localFont from "next/font/local";
 import "./globals.css";
-
-const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -16,7 +14,17 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body>
+        {/* App Layout */}
+        <div className="min-h-dvh grid grid-rows-[auto_1fr_auto]">
+          <header className="h-[88px] bg-slate-700 p-2">header</header>
+          <main>
+            <div>{children}</div>
+          </main>
+
+          <footer>footer</footer>
+        </div>
+      </body>
     </html>
   );
 }
