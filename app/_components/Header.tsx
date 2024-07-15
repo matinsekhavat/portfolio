780fe801abc5e99fd8adccb943b34268504870de
matinsekhavat <matinhyphen@gmail.com>
2024-07-16 01:04:46 +0330

    fix fileDown icon size fix

--- app/_components/Header.tsx
+++ app/_components/Header.tsx
@@ -1,7 +1,26 @@
+import { AlignJustify } from "lucide-react";
 import { ReactNode } from "react";
-
+import ButtonBadge from "./ButtonBadge";
+import { FileDown } from "lucide-react";
+import Nav from "./Nav";
 function Header() {
-  return <div>Header</div>;
+  // Header Design
+  return (
+    <header className=" container-fluid flex items-center justify-between h-[88px] ">
+      {/* rightSide Flow */}
+      <div className="flex items-center ">
+        <h1 className="text-xl font-semibold">نمونه کار من</h1>
+        <Nav />
+      </div>
+
+      {/* left side flow */}
+      <ButtonBadge>
+        <span className="flex items-center gap-2">
+          <FileDown size={20} /> دانلود رزومه
+        </span>
+      </ButtonBadge>
+    </header>
+  );
 }
 
 export default Header;
