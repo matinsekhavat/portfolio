26285607ee36d2fa381f4678e4ed30da6e6f28f6
matinsekhavat <matinhyphen@gmail.com>
2024-07-15 20:59:27 +0330

    make overLay component

---
+++ app/_components/Overlay.tsx
@@ -0,0 +1,19 @@
+import { ReactNode } from "react";
+
+type OverlayProps = {
+  children: ReactNode;
+  show: boolean;
+};
+function Overlay({ children, show = false }: OverlayProps) {
+  return (
+    <div
+      className={`${
+        show ? "fixed top-0 left-0 right-0 bottom-0 bg-black/50" : ""
+      }`}
+    >
+      {children}
+    </div>
+  );
+}
+
+export default Overlay;
