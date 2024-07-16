647ebe3b97c681cf753b1b54966c8c0c2d4c7928
matinsekhavat <matinhyphen@gmail.com>
2024-07-16 20:53:08 +0330

    fix some text font-size in hero

--- app/page.tsx
+++ app/page.tsx
@@ -8,23 +8,25 @@ function page() {
     <div>
       {/* Hero Section */}
       <section className="container-fluid">
-        <div className="grid my-10 md:grid-cols-[1.2fr_1fr]">
+        <div className="grid my-10 md:grid-cols-[1.1fr_1fr]">
           {/* intro section */}
           <div className="relative">
-            <div className="text-center space-y-8 hero-linear md:text-right">
-              <h3 className="text-4xl font-semibold">من متین هستم</h3>
-              <p className="text-3xl text-accent-200 font-semibold relative">
+            <div className="text-center  hero-linear md:text-right">
+              <h3 className="text-4xl font-semibold md:text-[35px] ">
+                من متین هستم
+              </h3>
+              <p className="text-3xl text-accent-200 font-semibold relative  my-6 md:text-4xl  ">
                 برنامه نویس + <br />
                 طراح رابط کاربری و تجربه کاربری
               </p>
-              <p className="text-lg text-justify sm:text-center md:text-right">
+              <p className="text-lg text-justify sm:text-center md:text-right md:text-xl my-8 md:my-16">
                 فردی پرشور و مشتاق به تعامل با افراد مختلف هستم. بیش از ۸ سال
                 سابقه فعالیت در زمینه دیجیتال مارکتینگ و همچنین بخش خدمات
                 مشتریان در صنعت خرده‌فروشی آنلاین و زیرساخت‌های اینترنتی را
                 دارم.
               </p>
             </div>
-            <div className="flex items-center gap-2 my-8 flex-wrap justify-center md:justify-start ">
+            <div className="flex items-center gap-2 my-20 flex-wrap justify-center md:justify-start ">
               <ButtonBadge>
                 <span className="text-lg">دانلود پورتفولیو</span>
               </ButtonBadge>
@@ -43,12 +45,12 @@ function page() {
             </div>
           </div>
           {/* Hero Image */}
-          <div className="relative">
+          <div className="relative  ">
             <Image
               src="/hero-image.webp"
               fill
               alt="header-image"
-              className="object-cover size-full"
+              className=" sm:object-contain mx-auto "
             />
           </div>
         </div>
