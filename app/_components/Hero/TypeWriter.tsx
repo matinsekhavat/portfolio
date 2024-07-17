00cc6dacedf76c6310a628d3132633d3ff8fcf00
matinsekhavat <matinhyphen@gmail.com>
2024-07-17 16:58:35 +0330

    add more flexibility to type writer component - loops controll

--- app/_components/Hero/TypeWriter.tsx
+++ app/_components/Hero/TypeWriter.tsx
@@ -1,13 +1,22 @@
 "use client";
 import { useEffect, useState, useMemo, useCallback } from "react";
+
 type TypeWriterProps = {
   textList: string[];
   speed: number;
+  timeLoops?: number | false; // Add timeLoops prop
 };
-export default function TypeWriter({ textList, speed = 120 }: TypeWriterProps) {
+
+export default function TypeWriter({
+  textList,
+  speed = 120,
+  timeLoops = false,
+}: TypeWriterProps) {
   const [index, setIndex] = useState(0);
   const [isDeleting, setIsDeleting] = useState(false);
   const [textIndex, setTextIndex] = useState(0);
+  const [loop, setLoop] = useState(0);
+  const [isTyping, setIsTyping] = useState(true);
 
   const newText = useMemo(() => {
     const currentText = textList[textIndex];
@@ -15,92 +24,49 @@ export default function TypeWriter({ textList, speed = 120 }: TypeWriterProps) {
   }, [textList, textIndex, index]);
 
   const intervalCallback = useCallback(() => {
-    const currentText = textList[textIndex];
+    if (!isTyping) {
+      return;
+    }
 
-    if (!isDeleting) {
-      if (index < currentText.length) {
-        setIndex((prev) => prev + 1);
-      } else {
-        setIsDeleting(true);
-      }
-    } else {
-      if (index > 0) {
-        setIndex((prev) => prev - 1);
-      } else {
-        setIsDeleting(false);
-        if (textIndex < textList.length - 1) {
-          setTextIndex((prev) => prev + 1);
+    const currentText = textList[textIndex];
+    if (timeLoops === false || loop < timeLoops) {
+      if (!isDeleting) {
+        if (index < currentText.length) {
+          setIndex((prev) => prev + 1);
         } else {
-          setTextIndex(0);
+          setIsDeleting(true);
+        }
+      } else {
+        if (index > 0) {
+          setIndex((prev) => prev - 1);
+        } else {
+          setIsDeleting(false);
+          if (textIndex < textList.length - 1) {
+            setTextIndex((prev) => prev + 1);
+          } else {
+            setTextIndex(0);
+            setLoop((prev) => prev + 1);
+          }
         }
       }
+    } else {
+      setIsTyping(false);
     }
-  }, [textList, index, isDeleting, textIndex]);
+  }, [textList, index, isDeleting, textIndex, loop, isTyping, timeLoops]);
 
   useEffect(() => {
-    const interval = setInterval(intervalCallback, speed);
+    if (!isTyping) {
+      return;
+    }
 
+    const interval = setInterval(intervalCallback, speed);
     return () => clearInterval(interval);
-  }, [intervalCallback]);
+  }, [intervalCallback, speed, isTyping]);
 
   return (
-    <div>
-      {newText}
-      {/* Add a caret at the end of the newText to simulate a typing cursor */}
-      <span>|</span>
-    </div>
+    <>
+      {isTyping ? newText : textList[0]}
+      <span className="blink">|</span>
+    </>
   );
 }
-//  v stop after
-// "use client";
-// import { useEffect, useState } from "react";
-
-// export default function TypeWriter({ textList }: { textList: string[] }) {
-//   const [newText, setNewText] = useState("");
-//   const [index, setIndex] = useState(0);
-//   const [isDeleting, setIsDeleting] = useState(false);
-//   const [textIndex, setTextIndex] = useState(0); // Track the current string in the textList
-//   const [isTyping, setIsTyping] = useState(true); // Track if typing effect should continue
-
-//   useEffect(() => {
-//     if (!isTyping) {
-//       setNewText(textList[0]);
-//       return;
-//     }
-
-//     const interval = setInterval(() => {
-//       const currentText = textList[textIndex];
-
-//       if (!isDeleting) {
-//         if (index < currentText.length) {
-//           setNewText((prev) => prev + currentText.charAt(index));
-//           setIndex((prev) => prev + 1);
-//         } else {
-//           setIsDeleting(true);
-//         }
-//       } else {
-//         if (index > 0) {
-//           setNewText((prev) => prev.slice(0, -1));
-//           setIndex((prev) => prev - 1);
-//         } else {
-//           setIsDeleting(false);
-//           if (textIndex < textList.length - 1) {
-//             setTextIndex((prev) => prev + 1);
-//           } else {
-//             setIsTyping(false);
-//           }
-//         }
-//       }
-//     }, 100);
-
-//     return () => clearInterval(interval);
-//   }, [textList, index, isDeleting, textIndex, isTyping]);
-
-//   return (
-//     <div>
-//       Hello I am {newText}
-//       {/* Add a caret at the end of the newText to simulate a typing cursor */}
-//       <span>|</span>
-//     </div>
-//   );
-// }
