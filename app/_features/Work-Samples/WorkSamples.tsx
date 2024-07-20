05904fb5e6c9b12ab3f822118204eb8b2629bb94
matinsekhavat <matinhyphen@gmail.com>
2024-07-20 17:41:09 +0330

    create and complete WorkSamle component including SamplesButton

--- app/_features/Work-Samples/WorkSamples.tsx
+++ app/_features/Work-Samples/WorkSamples.tsx
@@ -1,5 +1,33 @@
+import HeadTitle from "@/app/_components/HeadTitle";
+import WorkButtons from "./WorkButtons";
+
 function WorkSamples() {
-  return <div id="worksamples"></div>;
+  return (
+    <>
+      <div id="worksamples">
+        <HeadTitle
+          title="نمونه کار ها"
+          description="پروژه هایی که تاکنون انجام داده ام..."
+        />
+      </div>
+      {/* Selection List */}
+      <div className="flex items-center justify-center gap-2">
+        <WorkButtons paramsKey="cat" value="all">
+          همه
+        </WorkButtons>
+
+        <WorkButtons paramsKey="cat" value="application">
+          طراحی اپلیکیشن
+        </WorkButtons>
+        <WorkButtons paramsKey="cat" value="website">
+          طراحی وبسایت
+        </WorkButtons>
+        <WorkButtons paramsKey="cat" value="ui">
+          ui/ux
+        </WorkButtons>
+      </div>
+    </>
+  );
 }
 
 export default WorkSamples;
