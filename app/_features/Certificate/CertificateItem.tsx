7d3e8478b03f49896dd937c2a700f868ea0a6e9e
matinsekhavat <matinhyphen@gmail.com>
2024-07-20 23:53:52 +0330

    implement all certificate ui and validation props in ts

--- app/_features/Certificate/CertificateItem.tsx
+++ app/_features/Certificate/CertificateItem.tsx
@@ -16,13 +16,13 @@ function CertificateItem({
   description,
 }: CertificateItemProps) {
   return (
-    <div className="flex items-center gap-2 border border-accent-200 rounded-3xl p-4 overflow-hidden">
-      <div className="w-20">
-        <Image src={logoSrc} width={60} height={60} alt={logoAlt} />
+    <div className="flex items-center gap-3 border border-stone-700 rounded-xl p-4 overflow-hidden">
+      <div>
+        <Image src={logoSrc} width={50} height={50} alt={logoAlt} />
       </div>
-      <div className="flex-1 flex flex-col">
+      <div className="flex-1 flex flex-col gap-4">
         <h1 className="text-lg font-bold text-white">{title}</h1>
-        <p className="text-accent-800 text-sm">{description}</p>
+        <p className="text-stone-500 font-semibold text-sm">{description}</p>
       </div>
     </div>
   );
