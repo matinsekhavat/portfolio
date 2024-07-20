8979317dd059f407149e29cc086d6af17a1ccefc
matinsekhavat <matinhyphen@gmail.com>
2024-07-20 15:52:03 +0330

    implement and use all skillIsItem into skills component

--- app/_features/Skills/Skills.tsx
+++ app/_features/Skills/Skills.tsx
@@ -15,12 +15,66 @@ function Skills() {
         </p>
       </div>
       {/* Skills List */}
-      <div className="grid grid-cols-2">
+      <div className="grid grid-cols-2 gap-x-6 sm:grid-cols-3 md:grid-cols-5">
+        <SkillsItems
+          imageSrc="/html-logo.png"
+          imageAlt="HTML-logo"
+          percentage={98}
+          title="HTML"
+        />{" "}
+        <SkillsItems
+          imageSrc="/css-logo.png"
+          imageAlt="css-logo"
+          percentage={98}
+          title="CSS"
+        />{" "}
+        <SkillsItems
+          imageSrc="/sass-logo.png"
+          imageAlt="sass-logo"
+          percentage={79}
+          title="SCSS"
+        />
+        <SkillsItems
+          imageSrc="/tailwindcss-logo.png"
+          imageAlt="tailwindcss-logo"
+          percentage={95}
+          title="tailwindCSS"
+        />
+        <SkillsItems
+          imageSrc="/javascript-logo.png"
+          imageAlt="javascript-logo"
+          percentage={97}
+          title="Javascript"
+        />
+        <SkillsItems
+          imageSrc="/typescript-logo.png"
+          imageAlt="typescript-logo"
+          percentage={87}
+          title="TypeScript"
+        />
         <SkillsItems
           imageSrc="/react-logo.png"
           imageAlt="react-logo"
-          percentage={100}
+          percentage={95}
           title="React.JS"
+        />
+        <SkillsItems
+          imageSrc="/next-logo.png"
+          imageAlt="Next.js-logo"
+          percentage={85}
+          title="Next.JS"
+        />
+        <SkillsItems
+          imageSrc="/git-logo.png"
+          imageAlt="GIT-logo"
+          percentage={89}
+          title="GIT"
+        />
+        <SkillsItems
+          imageSrc="/figma-logo.png"
+          imageAlt="Figma-logo"
+          percentage={50}
+          title="figma"
         />
       </div>
     </div>
