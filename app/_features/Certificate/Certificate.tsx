92632c84c70582a9e07619b46241d8139ba48d25
matinsekhavat <matinhyphen@gmail.com>
2024-07-20 23:37:40 +0330

    implement and use CertificateItems into certificate Component => one instance

--- app/_features/Certificate/Certificate.tsx
+++ app/_features/Certificate/Certificate.tsx
@@ -1,27 +1,18 @@
 import Image from "next/image";
 import React from "react";
+import CertificateItem from "./CertificateItem";
 
 function Certificate() {
   return (
     <div className="bg-primary-850 grid grid-cols-2  ">
-      <div className="flex items-center gap-2 border border-accent-200 rounded-3xl p-4 overflow-hidden">
-        <div className="w-20">
-          <Image
-            src="/google-logo.webp"
-            width={70}
-            height={70}
-            alt="google logo"
-          />
-        </div>
-        <div className="flex-1 flex flex-col ">
-          <h1 className="text-lg font-bold text-white">
-            گواهینامه Google Digital Garage
-          </h1>
-          <p className="text-accent-800 text-sm">
-            اصول دیجیتال مارکتینگ (1390)
-          </p>
-        </div>
-      </div>
+      <CertificateItem
+        logoSrc="/google-logo.webp"
+        logoWidth={70}
+        logoHeight={70}
+        logoAlt="google logo"
+        title="گواهینامه Google Digital Garage"
+        description="اصول دیجیتال مارکتینگ (1390)"
+      />
     </div>
   );
 }
