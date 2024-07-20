158f927c0771c3fea176cca989b991db3ae89213
matinsekhavat <matinhyphen@gmail.com>
2024-07-20 16:03:38 +0330

    implement scroll behavior  and scroll padding for HTML tag elemnent

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,8 +15,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" dir="rtl">
-      <body className={`bg-primary-900 text-white font-dana ${dana.variable}`}>
+    <html lang="en" dir="rtl" className="scroll-smooth scroll-p-8">
+      <body
+        className={`  bg-primary-900 text-white font-dana ${dana.variable}`}
+      >
         {/* App Layout */}
         <div className="min-h-dvh grid grid-rows-[auto_1fr_auto]">
           <Header />
