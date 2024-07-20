005e140f2b3541f6b75d56139eedd76bbf520352
matinsekhavat <matinhyphen@gmail.com>
2024-07-20 23:35:55 +0330

    make first schema of certificate content

--- app/_features/Certificate/Certificate.tsx
+++ app/_features/Certificate/Certificate.tsx
@@ -1,7 +1,29 @@
+import Image from "next/image";
 import React from "react";
 
 function Certificate() {
-  return <div>Certificate</div>;
+  return (
+    <div className="bg-primary-850 grid grid-cols-2  ">
+      <div className="flex items-center gap-2 border border-accent-200 rounded-3xl p-4 overflow-hidden">
+        <div className="w-20">
+          <Image
+            src="/google-logo.webp"
+            width={70}
+            height={70}
+            alt="google logo"
+          />
+        </div>
+        <div className="flex-1 flex flex-col ">
+          <h1 className="text-lg font-bold text-white">
+            گواهینامه Google Digital Garage
+          </h1>
+          <p className="text-accent-800 text-sm">
+            اصول دیجیتال مارکتینگ (1390)
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Certificate;
