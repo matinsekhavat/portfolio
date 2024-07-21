4dccc2ea2f45a8c284e590055c999edb5759b6bb
matinsekhavat <matinhyphen@gmail.com>
2024-07-21 13:05:05 +0330

    import and use HeadTitle.tsx component at ui Top level of articles.tsx file

--- app/_features/Articles/Articles.tsx
+++ app/_features/Articles/Articles.tsx
@@ -1,5 +1,14 @@
+import HeadTitle from "@/app/_components/HeadTitle";
+
 function Articles() {
-  return <div>Articles</div>;
+  return (
+    <div>
+      <HeadTitle
+        title="مقالات"
+        description="تلاش برای داشتن اطلاعات کاربردی، به روز و جامع"
+      />
+    </div>
+  );
 }
 
 export default Articles;
