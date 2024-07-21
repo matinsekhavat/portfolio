834b171220a81036779d0624628a52f48165efbb
matinsekhavat <matinhyphen@gmail.com>
2024-07-21 14:00:05 +0330

    add arrowLeft icon from Luicde-icon  in articles Page

--- app/_features/Articles/Articles.tsx
+++ app/_features/Articles/Articles.tsx
@@ -1,12 +1,55 @@
 import HeadTitle from "@/app/_components/HeadTitle";
+import { e2p } from "@/app/_utils/replaceNumber";
+import { ArrowLeft, Calendar } from "lucide-react";
+import Image from "next/image";
 
 function Articles() {
   return (
-    <div>
+    <div id="#articles">
       <HeadTitle
         title="مقالات"
         description="تلاش برای داشتن اطلاعات کاربردی، به روز و جامع"
       />
+      {/* Articles List Wrapper */}
+      <div className="grid grid-cols-1 sm:grid-cols-2">
+        {/* item */}
+        <div className="p-2 rounded-xl bg-primary-850 ">
+          {/* Upperside */}
+          <div className="relative w-full h-72">
+            <Image
+              alt=""
+              fill
+              src="/article-1.jpeg"
+              className=" object-cover rounded-xl overflow-hidden"
+            />
+            {/* absolute div */}
+            <div className="p-2 backdrop-blur-sm absolute top-2 right-2 rounded-full">
+              رزومه نویسی
+            </div>
+          </div>
+          {/* downer side */}
+          <div className="flex flex-col gap-4  mt-4">
+            <h3 className="text-2xl">
+              زمان نوشتن رزومه چه قوانینی را رعایت کنیم؟
+            </h3>
+            <p className="line-clamp-2 sm:line-clamp-3">
+              لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ، و با
+              استفاده از طراحان گرافیک است، چاپگرها و...
+            </p>
+            <div className="flex items-center justify-between">
+              <div className="flex items-center gap-1">
+                <Calendar />
+                <p>
+                  فروردین {e2p(14)}, {e2p(1403)}{" "}
+                </p>
+              </div>
+              <span>
+                <ArrowLeft size={30} />
+              </span>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
