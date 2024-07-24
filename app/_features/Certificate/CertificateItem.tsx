729e542e1f7c7420420ffd7986ce0d1ab2eb1ec0
matinsekhavat <matinhyphen@gmail.com>
2024-07-25 00:50:34 +0330

    make some text and align more responsive in small mobile queries

--- app/_features/Certificate/CertificateItem.tsx
+++ app/_features/Certificate/CertificateItem.tsx
@@ -16,13 +16,15 @@ function CertificateItem({
   description,
 }: CertificateItemProps) {
   return (
-    <div className="flex items-center gap-3 border border-stone-700 rounded-xl  p-4 hover:border-accent-800 transition-all cursor-help ">
+    <div className="flex flex-col xs:flex-row items-center gap-3 border border-stone-700 rounded-xl  p-4 hover:border-accent-800 transition-all cursor-help ">
       <div>
         <Image src={logoSrc} width={50} height={50} alt={logoAlt} />
       </div>
-      <div className="flex-1 flex flex-col gap-4">
-        <h1 className="text-lg font-bold text-white">{title}</h1>
-        <p className="text-stone-500 font-semibold text-sm">{description}</p>
+      <div className="flex-1 flex flex-col gap-4 text-center xs:text-start">
+        <h1 className="text-base xs:text-lg font-bold text-white">{title}</h1>
+        <p className="text-sm xs:text-base text-stone-500 font-semibold ">
+          {description}
+        </p>
       </div>
     </div>
   );
