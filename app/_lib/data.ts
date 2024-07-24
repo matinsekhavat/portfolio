3b4a9e2dbae999b497d1865a19faca09a0a0fae8
matinsekhavat <matinhyphen@gmail.com>
2024-07-24 12:45:31 +0330

    create file for ArticlesData

---
+++ app/_lib/data.ts
@@ -0,0 +1,67 @@
+export const articlesData = [
+  {
+    imageSrc: "/article-1.jpeg",
+    imageAlt: "Article Image",
+    category: "رزومه نویسی",
+    title: "زمان نوشتن رزومه چه قوانینی را رعایت کنیم؟",
+    description:
+      "لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ، و با استفاده از طراحان گرافیک است، چاپگرها و...",
+    date: "1403/01/14",
+    year: 1403,
+    month: "فروردین",
+    day: 14,
+    href: "/article",
+  },
+  {
+    imageSrc: "/article-2.jpeg",
+    imageAlt: "Article Image",
+    category: "طراحی گرافیک",
+    title: "چگونه یک طراح گرافیک خوب شویم؟",
+    description:
+      "لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ، و با استفاده از طراحان گرافیک است، چاپگرها و...",
+    date: "1402/12/25",
+    year: 1402,
+    month: "دی",
+    day: 25,
+    href: "/article2",
+  },
+  {
+    imageSrc: "/article-3.jpeg",
+    imageAlt: "Article Image",
+    category: "توسعه وب",
+    title: "چگونه یک وب سایت خوب بسازیم؟",
+    description:
+      "لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ، و با استفاده از طراحان گرافیک است، چاپگرها و...",
+    date: "1401/09/01",
+    year: 1401,
+    month: "شهریور",
+    day: 1,
+    href: "/article3",
+  },
+  {
+    imageSrc: "/article-4.jpeg",
+    imageAlt: "Article Image",
+    category: "بازاریابی دیجیتال",
+    title: "چگونه یک کمپین بازاریابی دیجیتال خوب راه اندازی کنیم؟",
+    description:
+      "لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ، و با استفاده از طراحان گرافیک است، چاپگرها و...",
+    date: "1400/06/15",
+    year: 1400,
+    month: "خرداد",
+    day: 15,
+    href: "/article4",
+  },
+  {
+    imageSrc: "/article-5.jpeg",
+    imageAlt: "Article Image",
+    category: "آموزش زبان",
+    title: "چگونه زبان انگلیسی را خوب یاد بگیریم؟",
+    description:
+      "لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ، و با استفاده از طراحان گرافیک است، چاپگرها و...",
+    date: "1399/03/20",
+    year: 1399,
+    month: "اسفند",
+    day: 20,
+    href: "/article5",
+  },
+];
