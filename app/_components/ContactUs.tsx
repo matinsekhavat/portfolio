901339bc046101b8fc1d1340302e09eaa4f756e6
matinsekhavat <matinhyphen@gmail.com>
2024-07-25 01:05:36 +0330

    make some text arrange looks better

--- app/_components/ContactUs.tsx
+++ app/_components/ContactUs.tsx
@@ -9,11 +9,11 @@ interface ContactUsProps {
 
 const ContactUs: React.FC<ContactUsProps> = ({ Icon, description, title }) => {
   return (
-    <div className="flex items-center gap-4 flex-col sm:flex-row ">
+    <div className="flex items-center gap-4 flex-col sm:flex-row  text-center sm:text-start">
       <span className="size-14 p-2 bg-accent-800 rounded-full flex items-center justify-center">
         {Icon}
       </span>
-      <div className="space-y-4">
+      <div className="space-y-4 ">
         <p className="text-gray-50 font-bold text-lg">{title}</p>
         <p className="">{e2p(description)}</p>
       </div>
