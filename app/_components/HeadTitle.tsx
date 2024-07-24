b9656be759686f528b3e6b67e8dd6331e59edbbd
matinsekhavat <matinhyphen@gmail.com>
2024-07-24 19:44:47 +0330

    make HeadTitle component more reusable

--- app/_components/HeadTitle.tsx
+++ app/_components/HeadTitle.tsx
@@ -2,11 +2,23 @@ import React from "react";
 type HeadTitlleProps = {
   title: string;
   description: string;
+  size?: "sm" | "base";
 };
-function HeadTitle({ title, description }: HeadTitlleProps) {
+
+const HeadTitleStyle: HeadTitleStyleProps = {
+  sm: "text-2xl text-center",
+  base: "text-5xl ",
+};
+type HeadTitleStyleProps = {
+  sm: string;
+  base: string;
+};
+function HeadTitle({ title, description, size = "base" }: HeadTitlleProps) {
   return (
     <div className="text-center">
-      <span className="inline-block text-5xl font-bold bg-clip-text text-transparent bg-gradient-to-r from-accent-200 to-accent-800 text-clip">
+      <span
+        className={`inline-block ${HeadTitleStyle[size]} font-bold bg-clip-text text-transparent bg-gradient-to-r from-accent-200 to-accent-800 text-clip`}
+      >
         {title}
       </span>
       <br />
