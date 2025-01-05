9d998d52547ada1a46d607a75e15848391339c51
matinsekhavat <matinhyphen@gmail.com>
2025-01-05 10:57:12 +0330

    feat:Add Google Tag Manager integration and update Chatbox initialization in layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import { dana } from "@/public/fonts/font";
 import Header from "./_components/Header";
 import Footer from "./_components/Footer";
 import Script from "next/script";
+import GoogleTagManager from "./_lib/GoogleTagManager";
 
 export const metadata: Metadata = {
   title: "پورتفولیو متین سخاوت",
@@ -18,24 +19,53 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" dir="rtl" className="scroll-smooth scroll-p-8">
+   <head>
+        {/* Google Tag Manager Script */}
+        <Script
+          id="gtm-script"
+          strategy="beforeInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-PKDDKZLJ');
+            `,
+          }}
+        />
+      </head>
       <body
         className={`  bg-primary-900 text-white font-dana ${dana.variable}`}
       >
-       <Script
-          id="mu-chat"
-          strategy="afterInteractive"
-          dangerouslySetInnerHTML={{
-            __html: `
-              import Chatbox from 'https://muchat-develop-widget.liara.run/embeds/dist/chatbox/index.js';
-              const widget = await Chatbox.initBubble({
-                agentId: 'cm1lstpon0002sk19xpn8u8cy',
-              });
-            `,
-          }}
-          type="module"
-        />
+      
+         <Script
+         id="mu-chat"
+    
+        type="module"
+        dangerouslySetInnerHTML={{
+          __html: `
+            import Chatbox from 'https://cdn.mu.chat/embeds/dist/chatbox/index.js?v=2';
+            Chatbox.initBubble({
+              agentId: 'cm0ozf3q802e69fcak851bjsw',
+              interface: {
+               position: 'right',
+              },
+            loadingStrategy: 'SEO_FRIENDLY',
+            });
+          `
+        }}
+      />
         {/* App Layout */}
         <div className="min-h-dvh grid grid-rows-[auto_1fr_auto]">
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-PKDDKZLJ"
+            height="0"
+            width="0"
+            style={{ display: 'none', visibility: 'hidden' }}
+          />
+        </noscript>
           <Header />
           <main>
             <div>{children}</div>
